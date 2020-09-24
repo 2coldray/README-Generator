877191fbc54651b894f6724b377c9b13bba55858
Ray Bray <rbray.bray2@gmail.com>
2020-09-23 22:36:21 -0400

    added notes/pseudo code to index.js file

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ const fs = require("fs");
 const markdown = require("./markdown.js")
 
 //Inquirer
-
+//Need inputs for title, description, installation, usage, and tests
+//Need list for licence as well as badge to appear
+//Github username inserted into link
+//Need user to import email address as well
